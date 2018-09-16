8ea25d034f2a918958b63f4b2f01537d55bdf31c
shambhubhujel <31980047+shambhubhujel@users.noreply.github.com>
2018-09-16 14:04:30 +1000

    SplashActivity
    +Added simple splash screen

---
+++ app/src/main/java/com/example/shambhu/discountmarketapp/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.shambhu.discountmarketapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView splashImg;
+    Animation animation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        splashImg = (ImageView) findViewById(R.id.splashImg);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        splashImg.startAnimation(animation);
+
+        Thread timer = new Thread() {
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        timer.start();
+
+    }
+}
